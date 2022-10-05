dbb4ba3e32f31c0ebbf1ba740ff5ed43d0a8c408
overlord <rexatuzie@gmail.com>
2022-10-05 19:50:54 +0400

    dasboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Header from '../components/headerDashboard';
 import LoginFooter from '../components/loginFooter';
 
@@ -11,18 +11,18 @@ const dashboard = () => {
   };
   return (
     <div>
-      <Header/>
-      <div className='container my-5'>
-        <div className='row justify-content-center'>
-          <div className='col-md-8 text-center'>
-            <h1>Hi, {user.name}</h1>
+      <Header />
+      <div className="container my-5">
+        <div className="row justify-content-center">
+          <div className="col-md-8 text-center">
+            <h1>Hi, {user.firstname}</h1>
             <Link to={'/login'} onClick={handleClick}>
               back
             </Link>
           </div>
         </div>
       </div>
-      <LoginFooter/>
+      <LoginFooter />
     </div>
   );
 };

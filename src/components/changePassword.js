10309dcecc22e6d38dd91e15eb19ef6675e6e477
overlord <rexatuzie@gmail.com>
2022-10-20 14:58:16 +0400

    thurs commit 40

--- src/components/changePassword.js
+++ src/components/changePassword.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 function ChangePassword() {
+  const [resetPassword, setRestPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const { id } = useParams();
+
+  console.log(id);
+
   return (
     <section className="login-form">
       <div className="container">
@@ -10,15 +19,33 @@ function ChangePassword() {
               <div className="passform">
                 <h4 className="text-center">Change your password</h4>
                 <h6 className="mt-3 text-center">Enter your new password</h6>
-                <form className='row mt-5'>
+                <form className="row mt-5">
                   <div className="mb-4">
-                    <input type="password" id="passwordreset" className="form-control" placeholder="Enter new password" required/>
+                    <input
+                      type="password"
+                      id="passwordreset"
+                      className="form-control"
+                      placeholder="Enter new password"
+                      required
+                    />
                   </div>
                   <div className="mb-4">
-                    <input type="password" id="passwordresetconfirm" className="form-control" placeholder="Confirm new password" required/>
+                    <input
+                      type="password"
+                      id="passwordresetconfirm"
+                      className="form-control"
+                      placeholder="Confirm new password"
+                      required
+                    />
                   </div>
                   <div className="">
-                    <button type="button" className="brand-bg text-white px-4 py-3 fs-6 rounded-pill" style={{width: "100%"}}>Reset Password</button>
+                    <button
+                      type="button"
+                      className="brand-bg text-white px-4 py-3 fs-6 rounded-pill"
+                      style={{ width: '100%' }}
+                    >
+                      Reset Password
+                    </button>
                   </div>
                 </form>
               </div>
@@ -27,7 +54,7 @@ function ChangePassword() {
         </div>
       </div>
     </section>
-  )
+  );
 }
 
 export default ChangePassword;

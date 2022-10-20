05926bd472e76ca2b973fe4172a92fa18d1e5fca
Olamilekan Akindele <olalekan@astract.com.ng>
2022-10-20 15:22:39 +0100

    fixes

--- src/components/forgotForm.js
+++ src/components/forgotForm.js
@@ -84,7 +84,7 @@ function ForgotForm() {
                         className="brand-bg text-white px-4 py-3 fs-6 rounded-pill"
                         style={{ width: '100%' }}
                       >
-                        Register
+                        Send
                       </button>
                     </div>
                   )}

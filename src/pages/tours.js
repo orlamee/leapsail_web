802428eb570e5d2c353be3316cb5a2f27783e41f
Olamilekan Akindele <olalekan@astract.com.ng>
2022-10-27 12:50:40 +0100

    footer-tour

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -2,12 +2,15 @@ import React from "react";
 import "../css/tours.css";
 import Header from "../components/navBar";
 import TourHero from "../components/tourHero";
+import Footer from "../components/footer";
+
 
 function Tours() {
   return (
     <div>
       <Header/>
       <TourHero/>
+      <Footer/>
     </div>
   )
 }

ebc51ad02df9544af6a9bfc66858b491355d332b
Olamilekan Akindele <olalekan@astract.com.ng>
2022-10-28 15:35:11 +0100

    404

--- src/components/errorPage.js
+++ src/components/errorPage.js
@@ -1,14 +1,16 @@
 import React from "react";
+import error from "../img/error.jpg";
 
 function ErrorPage() {
   return (
     <section className="errorpage">
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-md-7">
-            <h1 className="mb-3">Ooooops!!</h1>
-            <p className="mb-5">We cannot seem to find the page you are looking for</p>
-            <a className='brand-bg text-white px-4 fs-6 py-3 rounded-pill' href="/">Back Home</a>
+          <div className="col-md-12 text-center">
+            <img src={error} alt="" className="img-fluid" width={350}/>
+            <h1 className="mb-3">404 - Page Not Found</h1>
+            <p className="mb-5">Ooooops!!! We cannot seem to find the page you are looking for</p>
+            <a className='brand-bg text-white px-4 fs-6 py-3 rounded-pill' href="/">Back to Home</a>
           </div>
         </div>
       </div>

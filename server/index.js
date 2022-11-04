17fe1b5a6e4ae273f976cf50fb799a9573c2aaf5
backend dev <developer@leapsail.com.ng>
2022-11-04 09:10:21 -0700

    fri test commit

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-const authRoute = require('./routes/auth.js');
-const userRoute = require('./routes/user.js');
-const contactRoute = require('./routes/contact.js');
-const employeeRoute = require('./routes/employee.js');
-const educationRoute = require('./routes/education.js');
+const authRoute = require("./routes/auth.js");
+const userRoute = require("./routes/user.js");
+const contactRoute = require("./routes/contact.js");
+const employeeRoute = require("./routes/employee.js");
+const educationRoute = require("./routes/education.js");
 
 const app = express();
 dotenv.config();
@@ -16,7 +16,7 @@ dotenv.config();
 const connectDB = () => {
   mongoose
     .connect(process.env.MONGO)
-    .then(() => console.log('DB connected'))
+    .then(() => console.log("DB connected"))
     .catch((err) => console.error(err));
 };
 
@@ -24,15 +24,15 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/leapsail/api/auth', authRoute);
-app.use('/leapsail/api/user', userRoute);
-app.use('/leapsail/api/contact', contactRoute);
-app.use('/leapsail/api/employee', employeeRoute);
-app.use('/leapsail/api/education', educationRoute);
+app.use("/leapsail/api/auth", authRoute);
+app.use("/leapsail/api/user", userRoute);
+app.use("/leapsail/api/contact", contactRoute);
+app.use("/leapsail/api/employee", employeeRoute);
+app.use("/leapsail/api/education", educationRoute);
 
 app.use((err, req, res, next) => {
   const status = err.status || 500;
-  const message = err.message || 'Something went wrong';
+  const message = err.message || "Something went wrong";
 
   return res.status(status).json({
     success: false,
@@ -45,5 +45,5 @@ const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   connectDB();
-  console.log('Server connected');
+  console.log("Server connected...");
 });
